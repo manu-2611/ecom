e76d38ff2bfd087b619249ca187ceb0a5c5382b9
Manu Shrivastava <manushrivastava528@gmail.com>
2020-12-01 01:16:34 +0530

    second commit

--- shop/urls.py
+++ shop/urls.py
@@ -2,11 +2,47 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,name='Home'),
-    path('about/', views.about,name='AboutUs'),
-    path('contact/', views.contact,name='ContactUs'),
-    path('tracker/', views.tracker,name='TrackingStatus'),
-    path('search/', views.search,name='Search'),
-    path('productview/', views.productView,name='ProductView'),
-    path('checkout/', views.checkout,name='checkout'),
+    path("", views.index, name="Home"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="TrackingStatus"),
+    path("search/", views.search, name="Search"),
+    path("products/<int:myid>/", views.productView, name="ProductView"),
+    path("checkout/", views.checkout, name="Checkout"),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# from django.urls import path
+# from . import views
+#
+# urlpatterns = [
+#     path('', views.index,name='Home'),
+#     path('about/', views.about,name='AboutUs'),
+#     path('contact/', views.contact,name='ContactUs'),
+#     path('tracker/', views.tracker,name='TrackingStatus'),
+#     path('search/', views.search,name='Search'),
+#     path('products/<int:id>', views.productView, name='productView'),
+#     path('checkout/', views.checkout,name='checkout'),
+# ]
--- shop/views.py
+++ shop/views.py
@@ -1,29 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product, Contact
+from .models import Product
+from math import ceil
+
 # Create your views here.
 def index(request):
-    return render(request ,'shop/index.html')
+    # products = Product.objects.all()
+    # print(products)
+    # n= len(products)
+    # nslides= n//4+ ceil((n/4)-(n//4))
+    # params = {'no_of_products':nslides,'range':range(1,nslides),'product': products}
+    # allProds=[[products,range(1,nslides),len(products)],[products,range(1,nslides),len(products)]]
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds':allProds}
+    return render(request ,'shop/index.html',params)
 
 def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    return HttpResponse('contact')
-
+    if request.method=="POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+    return render(request, 'shop/contact.html')
 
 
 def tracker(request):
-    return HttpResponse('tracker')
-
+    return render(request, 'shop/tracker.html')
 
 
 def search(request):
-    return HttpResponse('search')
+    return render(request, 'shop/search.html')
 
-
-def productView(request):
-    return HttpResponse('prodView')
+def productView(request, myid):
+    # Fetch the product using the id
+    product = Product.objects.filter(id=myid)
 
 
 def checkout(request):
-    return HttpResponse('checkout')
+    return render(request, 'shop/checkout.html')
